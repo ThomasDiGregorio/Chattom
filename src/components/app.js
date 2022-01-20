aaf594d81a9b231bfeebf6f5cb78fc6e39c01daf
ThomasDiGregorio <digregoriothomas@yahoo.com>
2022-01-20 18:54:43 -0500

    second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <h1>Homepage</h1>
+        <h2>Navigation Bar</h2>
       </div>
     );
   }
