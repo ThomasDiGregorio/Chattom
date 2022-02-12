93e057004c5bd07e9c114f2fce8b9e8a583696b9
ThomasDiGregorio <digregoriothomas@yahoo.com>
2022-02-11 22:05:37 -0500

    created site info box with text

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 
 export default class Home extends Component {
   render() {
-    return <div className="home"></div>;
+    return (
+      <div className="home">
+        <div className="built-with">
+          <h3>Backend:</h3>
+          <h4>Python</h4>
+          <h3>Frontend:</h3>
+          <h4>React, Axios, JavaScript, UI/UX, SASS, SCSS</h4>
+        </div>
+      </div>
+    );
   }
 }
