c6bddf53aded02f2f060cc6ee55ce005ce9afe7b
ThomasDiGregorio <digregoriothomas@yahoo.com>
2022-02-15 11:31:25 -0500

    modified authentication file

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+const ReactCSSTG = React.addons.CSSTransitionGroup;
 export default class Authentication extends Component {
   constructor(props) {
     super(props);
